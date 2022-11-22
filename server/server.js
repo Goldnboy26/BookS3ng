5eb214d59b700ab067ee05e5c8a49854f7aeb6ee
Goldnboy26 <enrique.monje1994@yahoo.com>
2022-11-21 21:09:28 -0800

    adding deploy stuff

--- server/server.js
+++ server/server.js
@@ -28,12 +28,13 @@ const startApolloServer = async (typeDefs, resolvers) => {
 
   // Serve up static assets
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, 'build')));
   }
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
+  app.listen(9000);
 
   db.once('open', () => {
     app.listen(PORT, () => {
