55c20809d29fa908d2959ef5e0fa0299c205b569
Goldnboy26 <enrique.monje1994@yahoo.com>
2022-11-29 20:27:50 -0800

    add

--- server/server.js
+++ server/server.js
@@ -21,8 +21,8 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'build')));
 }
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build' , 'index.html'));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 app.listen(9000);
 
