aef8627e898b41e106eaf913de6744649c222c54
Goldnboy26 <enrique.monje1994@yahoo.com>
2022-12-18 22:04:42 -0800

    add appollo

--- server/server.js
+++ server/server.js
@@ -1,33 +1,37 @@
-//import packages and graphql schemas//
-const express = require('express');
-const path = require('path');
-const { ApolloServer } = require('apollo-server-express');
-const db = require('./config/connection');
-const { typeDefs, resolvers } = require('./schemas');
+const express = require("express");
+const path = require("path");
+const db = require("./config/connection");
+
+// add apollo server
+const { ApolloServer } = require("apollo-server-express");
+const { typeDefs, resolvers } = require("./schemas");
+const { authMiddleware } = require("./utils/auth");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-const server = new ApolloServer({
-  //pass in graphql typeDefs and resolvers for queries and mutations//
-  typeDefs,
-  resolvers,
-});
 
-server.applyMiddleware({ app });
+// add apollo middleware
+const startServer = async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
+
+startServer();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, '../client/build')));
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
 
-//connect front-end build with backend//
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
-
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`🌍 Now listening on localhost:${PORT}`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-  });
+db.once("open", () => {
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
